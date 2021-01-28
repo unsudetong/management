66e9bb1fe090f16a227e51013063f56d330ff9e6
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-01-28 18:51:16 +0900

    [BE] feat : API USER에 status code 추가 #13

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -17,7 +17,7 @@ class User {
         .send({ message: '유저의 목록입니다.', result: users });
     } catch (error) {
       console.error(error);
-      return res.send({
+      return res.status(401).send({
         message: '유저 정보를 가져오는 데에서 오류가 발생하였습니다.',
       });
     }
@@ -25,7 +25,7 @@ class User {
 
   static async createOneUser(req, res, next) {
     try {
-      const { STUDENT_ID, PASSWORD } = req.body;
+      const { STUDENT_ID, PASSWORD } = await req.body;
       if (!STUDENT_ID) {
         return res.status(401).send({ message: '학번을 다시 확인해주세요.' });
       }
@@ -45,21 +45,23 @@ class User {
         });
       }
 
+      // TODO : PASSWORD를 hash하는 함수를 반드시, 반드시! 만들 것.
       const newUser = await model.create({ STUDENT_ID, PASSWORD });
       return res.status(201).send({
-        message: '회원가입이 성공',
+        message: '회원가입에 성공하였습니다.',
         result: newUser,
       });
     } catch (error) {
       console.error(error);
+      return res.status(401).send({ message: '유저 생성에 실패하였습니다.' });
     }
   }
 
   static async deleteOneUser(req, res, next) {
     try {
-      const { STUDENT_ID, PASSWORD } = req.body;
+      const { STUDENT_ID, PASSWORD } = await req.body;
       const result = await model.destroy({
-        where: { STUDENT_ID: STUDENT_ID, PASSWORD: PASSWORD },
+        where: { STUDENT_ID, PASSWORD },
       });
       if (!!result) {
         return res.status(200).send({ message: '유저가 삭제되었습니다.' });
@@ -67,6 +69,7 @@ class User {
       return res.status(400).send({ message: '해당하는 유저가 없습니다.' });
     } catch (error) {
       console.error(error);
+      return res.status(401).send({ message: '유저 삭제에 실패하였습니다.' });
     }
   }
 }
