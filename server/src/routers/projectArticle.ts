22456a65f5ed3ae817a42775791549544d7d614a
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-03 17:57:57 +0900

    [BE] feat : projectArticle table 삭제 #16

--- server/src/routers/projectArticle.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/projectArticle';
-
-const router = express.Router();
-
-router.route('/').get(controller.getAllProjectArticle);
-
-export default router;
