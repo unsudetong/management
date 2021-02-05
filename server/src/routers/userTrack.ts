a6514213aee38069b673e47a63cedbec66f0eea3
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-05 16:34:04 +0900

    [BE] feat : 성능 개선을 위한 join 쿼리 추가 #16

--- server/src/routers/userTrack.ts
+++ server/src/routers/userTrack.ts
@@ -3,12 +3,13 @@ import controller from '../controllers/userTrack';
 
 const router = express.Router();
 
-router.route('/:USER_ID').get(controller.getAllTrackOfUser);
+router.route('/:TRACK_ID').get(controller.getAllArticlesOfProjectsOfTracks);
+// router.route('/:USER_ID').get(controller.getAllTrackOfUser);
 
 router
   .route('/')
   .get(controller.getAllUserTrack)
-  .post(controller.createOneUserTrack)
+  // .post(controller.createOneUserTrack)
   .delete(controller.deleteOneUserTrack);
 
 export default router;
