a0031cdf56e2235589f1769697ca83463b58f4a3
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-06 14:03:40 +0900

    [FE] feat : article의 CONTENTS를 띄우기 #4

--- client/src/components/templates/ArticleContents.tsx
+++ client/src/components/templates/ArticleContents.tsx
@@ -3,11 +3,32 @@ import styled from 'styled-components';
 
 const ArticleContents = (props: any): JSX.Element => {
   const curArticle = props.match.params.article_id;
+  const [article, setArticle]: any = useState([]);
+
+  const getArticle = () => {
+    const cookie = localStorage.getItem('cookie');
+    try {
+      fetch(process.env.REACT_APP_SERVER_ADDRESS + `/articles/${curArticle}`, {
+        method: 'GET',
+        headers: {
+          Authorization: cookie || '',
+        },
+      })
+        .then(res => res.json())
+        .then(res => setArticle(res.result));
+    } catch (error) {
+      throw new Error('authCheck error');
+    }
+  };
+
+  useEffect(() => {
+    getArticle();
+  }, []);
 
   return (
     <div>
-      <span>hello world.</span>
-      <span>this is {curArticle}</span>
+      <h1>TITLE : {article.TITLE}</h1>
+      <h2>CONTENTS : {article.CONTENTS}</h2>
     </div>
   );
 };
