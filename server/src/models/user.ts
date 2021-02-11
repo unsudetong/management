278b3fbcdcf2973a1e88013c77f1f7c4c72c835c
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-11 16:12:16 +0900

    [BE] fix : 유저의 탐색 조건을 ID에서 STUDENT_ID 로 변경 #14

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,7 +1,8 @@
 import Model from './model';
 
 const GET_QUERY = 'SELECT * FROM USERS';
-const GET_QUERY_WHERE = term => GET_QUERY + ` WHERE ID = ${term}`;
+const GET_QUERY_WHERE = term =>
+  GET_QUERY + ` WHERE STUDENT_ID = ${term} LIMIT 1`;
 const POST_QUERY = `INSERT INTO USERS SET ?`;
 const DELETE_QUERY = ID => `DELETE FROM USERS WHERE ID = ${ID}`;
 
