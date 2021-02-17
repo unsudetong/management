d129f0df1878c6f0dc40ce41654a3362058179c6
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-17 13:43:13 +0900

    [BE] fix : http test #0

--- server/src/bin/www.ts
+++ server/src/bin/www.ts
@@ -1,10 +1,10 @@
 import app from '../app';
 
+/*
 import https from 'https';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-console.log(__dirname);
 const option = {
   key: readFileSync(__dirname + '/../../keys/private.pem'),
   cert: readFileSync(__dirname + '/../../keys/public.pem'),
@@ -12,4 +12,11 @@ const option = {
 
 https.createServer(option, app).listen(4000, () => {
   console.log('HTTPS server listening on port ' + 4000);
+});
+
+*/
+
+import http from 'http';
+http.createServer(app).listen(4000, () => {
+  console.log('http server on port : ', 4000);
 });
