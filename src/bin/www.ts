bbfb9661a320f4538bfa12f06989cad7c118d4c4
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-25 16:56:42 +0900

    [BE] test : 서버 측에서도 https 적용이 필요한가?
    - 도메인에만 필요한 개념이 아닌가?
    - 로컬호스트에서 동작하고 있는데도?

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,42 +1,46 @@
 import app from '../app';
-import http, { Server } from 'http';
-import https from 'https';
-import fs from 'fs';
-import { Request, Response } from 'express';
+import http from 'http';
+// import https from 'https';
+// import fs from 'fs';
+// import { Request, Response } from 'express';
 
-const option =
-  process.env.NODE_ENV === 'production'
-    ? {
-        key: fs.readFileSync(
-          __dirname + '/../../../../cert/luckydata.domain.key.pem',
-          'utf-8',
-        ),
-        cert: fs.readFileSync(
-          __dirname + '/../../../../cert/luckydata.domain.pem',
-          'utf-8',
-        ),
-        ca: fs.readFileSync(
-          __dirname + '/../../../../cert/luckydata.domain.ca.pem',
-          'utf-8',
-        ),
-      }
-    : undefined;
+// const option =
+//   process.env.NODE_ENV === 'production'
+//     ? {
+//         key: fs.readFileSync(
+//           __dirname + '/../../../../cert/luckydata.domain.key.pem',
+//           'utf-8',
+//         ),
+//         cert: fs.readFileSync(
+//           __dirname + '/../../../../cert/luckydata.domain.pem',
+//           'utf-8',
+//         ),
+//         ca: fs.readFileSync(
+//           __dirname + '/../../../../cert/luckydata.domain.ca.pem',
+//           'utf-8',
+//         ),
+//       }
+//     : undefined;
 
-option
-  ? https.createServer(option, app).listen(443, () => {
-      console.log('https server on port : ', 443);
-    })
-  : undefined;
+// option
+//   ? https.createServer(option, app).listen(4000, () => {
+//       console.log('https server on port : ', 4000);
+//     })
+//   : undefined;
 
-option
-  ? http
-      .createServer((req: Request, res: Response) => {
-        res.writeHead(301, {
-          Location: 'https://' + req.headers['host'] + req.url,
-        });
-        res.end();
-      })
-      .listen(4000)
-  : http.createServer(app).listen(4000, () => {
-      console.log('http server on port : ', 4000);
-    });
+// option
+//   ? http
+//       .createServer((req: Request, res: Response) => {
+//         res.writeHead(301, {
+//           Location: 'https://' + req.headers['host'] + req.url,
+//         });
+//         res.end();
+//       })
+//       .listen(4001)
+//   : http.createServer(app).listen(4000, () => {
+//       console.log('http server on port : ', 4000);
+//     });
+
+http.createServer(app).listen(4000, () => {
+  console.log('http server on port : ', 4000);
+});
